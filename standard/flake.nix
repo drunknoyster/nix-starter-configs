99ed3d121450373c1537a80fda5f68ae3ffbda71
drunknoyster <121766647+drunknoyster@users.noreply.github.com>
2023-01-02 21:37:17 +0000

    Rev 02

--- standard/flake.nix
+++ standard/flake.nix
@@ -78,4 +78,18 @@
         };
       };
     };
+
+  # Enable Cachix 
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.m7.rs"
+      "https://hyprland.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.m7.rs:kszZ/NSwE/TjhOcPPQ16IuUiuRSisdiIwhKZCxguaWg="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+   ];
+ };
 }
