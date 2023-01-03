9b46f6fdc09becfd5b00d3adae2252b37d486b4a
drunknoyster <121766647+drunknoyster@users.noreply.github.com>
2023-01-03 06:31:29 +0000

    Fixed vscode uncomment

--- standard/home-manager/home.nix
+++ standard/home-manager/home.nix
@@ -6,7 +6,7 @@
   imports = [
     # If you want to use modules your own flake exports (from modules/home-manager):
      #outputs.homeManagerModules.example
-     outputs.homeManagerModules.vscode
+     #outputs.homeManagerModules.vscode
     # Or modules exported from other flakes (such as nix-colors):
     # inputs.nix-colors.homeManagerModules.default
 
@@ -58,8 +58,8 @@
   programs.home-manager.enable = true;
   programs.git = {
     enable = true;
-    userName = "drunknoyster"
-    userEmail = "jbeauter@protonmail.com"
+    userName = "drunknoyster";
+    userEmail = "jbeauter@protonmail.com";
     extraConfig = {
       credential.helper = "${pkgs.gitAndTools.gitFull}/bin/git-credential-libsecret";
     };
